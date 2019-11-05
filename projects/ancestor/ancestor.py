ca2e8c41f7f31e46395f2ac8d9a52ba72a7c84d9
Ang Xu <angxu1234@gmail.com>
2019-11-05 14:47:31 -0600

    commented MVP

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,23 +1,39 @@
 
 def earliest_ancestor(ancestors, starting_node):
+    # start a queue and add starting node
     q = []
     q.append([starting_node])
+    # initialize visited cache
     visited = set()
+    # initialize store of path ways
     store = []
+    # while queue is not empty
     while len(q) > 0:
+        # dequeue first path and add the path to store of path ways
         path = q.pop(0)
         store += [path]
         # print(path)
+
+        # grab last node from path
         v = path[-1]
         # print(v)
+
+        # if the node has not been visited
         if v not in visited:
+            # mark node as visited
             visited.add(v)
+            # loop through list of connections
             for tup in ancestors:
+                # if the node is a child (i.e. second element of tuple)
                 if v == tup[1]:
+                    # append the parent of the child to path and add to back of queue
                     q.append(path + [tup[0]])
+    # result after while loop is finished is the list that has the highest len
     result = max(store, key=len)
+    # if len is only 1, we know the list only has original element and no parents
     if len(result) == 1:
         return -1
+    # else return last element of the result list
     else:
         return result[-1]
 
